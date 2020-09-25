1aa8921a8775be75d99b78a7fb3632255a30c47a
sfoutz0205 <68661461+sfoutz0205@users.noreply.github.com>
2020-09-25 17:05:48 -0600

    generate HTML

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
+const fs = require('fs');
+const generatePage = require('./src/page-template');
+
 const profileDataArgs = process.argv.slice(2, process.argv.length);
 
+const [name, github] = profileDataArgs;
 
-const printProfileData = profileDataArr => {
-    profileDataArr.forEach((profileItem) => console.log(profileItem));
-};
 
-printProfileData(profileDataArgs);
+
+fs.writeFile('index.html', generatePage(name, github), err => {
+    if (err) throw new Error(err);
+
+    console.log('Portfolio complete! Check out index.html to see the output!');
+});
