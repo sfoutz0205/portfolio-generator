8612970f8c326d6d93a7bc08006a45025af82706
sfoutz0205 <68661461+sfoutz0205@users.noreply.github.com>
2020-09-28 18:46:01 -0600

    fully functioning

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const { writeFile, copyFile } = require('./utils/generate-site.js')
 const generatePage = require('./src/page-template');
 const inquirer = require('inquirer');
 const promptUser = () => { 
@@ -122,65 +123,37 @@ const promptProject = portfolioData => {
     });
 };
 
-const mockData = 
-
-    {
-    name: 'Sophie Foutz',
-    github: 'sfoutz0205',
-    confirmAbout: true,
-    about:
-      'Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque. Nulla eget fringilla nulla. Integer gravida magna mi, id efficitur metus tempus et.',
-    projects: [
-      {
-        name: 'Run Buddy',
-        description:
-          'Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque. Nulla eget fringilla nulla. Integer gravida magna mi, id efficitur metus tempus et. Nam fringilla elit dapibus pellentesque cursus.',
-        languages: ['HTML', 'CSS'],
-        link: 'https://github.com/lernantino/run-buddy',
-        feature: true,
-        confirmAddProject: true
-      },
-      {
-        name: 'Taskinator',
-        description:
-          'Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque. Nulla eget fringilla nulla. Integer gravida magna mi, id efficitur metus tempus et. Nam fringilla elit dapibus pellentesque cursus.',
-        languages: ['JavaScript', 'HTML', 'CSS'],
-        link: 'https://github.com/lernantino/taskinator',
-        feature: true,
-        confirmAddProject: true
-      },
-      {
-        name: 'Taskmaster Pro',
-        description:
-          'Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque. Nulla eget fringilla nulla. Integer gravida magna mi, id efficitur metus tempus et. Nam fringilla elit dapibus pellentesque cursus.',
-        languages: ['JavaScript', 'jQuery', 'CSS', 'HTML', 'Bootstrap'],
-        link: 'https://github.com/lernantino/taskmaster-pro',
-        feature: false,
-        confirmAddProject: true
-      },
-      {
-        name: 'Robot Gladiators',
-        description:
-          'Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque.',
-        languages: ['JavaScript'],
-        link: 'https://github.com/lernantino/robot-gladiators',
-        feature: false,
-        confirmAddProject: false
-      }
-    ]
-};
-
-const pageHTML = generatePage(mockData);
-
 
 promptUser()
-//     .then(promptProject)
-//     .then(portfolioData => {
-//         const pageHTML = generatePage(portfolioData);
+    .then(promptProject)
+    .then(portfolioData => {
+        return generatePage(portfolioData);
+    })
+    .then(pageHTML => {
+        return fs.writeFile(pageHTML);
+    })
+    .then(writeFileResponse => {
+        console.log(writeFileResponse);
+        return fs.copyFile();
+    })
+    .then(copyFileResponse => {
+        console.log(copyFileResponse);
+    })
+    .catch(err => {
+        console.log(err);
+    });
 
-        fs.writeFile('index.html', pageHTML, err => {
-            if (err) throw new Error(err);
-        });
-// });
+
+    //         fs.copyFile('./src/style.css', './dist/style.css', err => {
+    //             if (err) {
+    //                 console.log(err);
+    //                 return;
+    //             }
+    //             console.log('Style sheet copied successfully');
+    //         })
+    //     });
+    // });
+
+    
 
 
--- src/page-template.js
+++ src/page-template.js
@@ -21,16 +21,16 @@ const generateProjects = projectsArr => {
       .filter(({ feature }) => feature)
       .map(({ name, description, languages, link}) => {
         return `
-          <div class="col-12 col-md-6 mb-2 bg-dark text-light p-3 flex-column">
-            <h3 class"portfolio-item-title text-light">${name}</h3>
-            <h5 class="portfolio-languages">
-              Built With: 
-              ${languages.join(', ')}
-            </h5>
-            <p>${description}</p>
-            <a href="${link}" class="btn mt-auto"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
-          </div>
-        `;
+        <div class="col-12 mb-2 bg-dark text-light p-3">
+        <h3 class="portfolio-item-title text-light">${name}</h3>
+        <h5 class="portfolio-languages">
+            Built With:
+            ${languages.join(', ')}
+          </h5>
+          <p>${description}</p>
+          <a href="${link}" class="btn mt-auto"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
+        </div>
+      `;
       })
       .join('')}
 
@@ -74,7 +74,7 @@ module.exports = templateData => {
 
     <body>
       <header>
-        <div class="container flex-row justify space between align-center py-3">
+        <div class="container flex-row justify space-between align-center py-3">
           <h1 class="page-title text-secondary bg-dark py-2 px3">${header.name}</h1>
           <nav class="flex-row">
             <a class="ml-2 my-1 px-2 py-1 bg-secondary text-dark" href="https://github.com/${
